dab20abafcff1f50e2f6077c401e3dd222cf3d83
JWShuff <16825267+JWShuff@users.noreply.github.com>
2021-06-08 17:56:35 -0400

    Jonas and Jack's bank account ~Part 3

---
+++ modules/owner.py
@@ -0,0 +1,51 @@
+import csv
+import os
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, "../support/owners.csv")
+
+
+class Owner:
+    def __init__(self, id, last_name, first_name, street_address, city, state):
+        self.id = id
+        self.last_name = last_name
+        self.first_name = first_name
+        self.street_address = street_address
+        self.city = city
+        self.state = state
+
+    @classmethod
+    def get_all_owners(cls):
+        with open(path) as owners_file:
+            reader = csv.DictReader(owners_file)
+            owners_list = []
+            for row in reader:
+                new_owner = Owner(int(row["id"]), row["last_name"], row["first_name"],
+                                  row["street_address"], row["city"], row["state"])
+                owners_list.append(new_owner)
+        return owners_list
+        import csv
+
+
+my_path = os.path.abspath(os.path.dirname(__file__))
+path = os.path.join(my_path, "../support/owners.csv")
+
+
+class Owner:
+    def __init__(self, id, last_name, first_name, street_address, city, state):
+        self.id = id
+        self.last_name = last_name
+        self.first_name = first_name
+        self.street_address = street_address
+        self.city = city
+        self.state = state
+
+    @classmethod
+    def get_all_owners(cls):
+        with open(path) as owners_file:
+            reader = csv.DictReader(owners_file)
+            owners_list = []
+            for row in reader:
+                new_owner = Owner(int(row["id"]), row["last_name"], row["first_name"],
+                                  row["street_address"], row["city"], row["state"])
+                owners_list.append(new_owner)
+        return owners_list
